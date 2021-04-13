45a904a231261d39e09fd0e3b323740d39b55001
anonymouse <anonymouse@x.com>
2021-04-13 15:28:35 +0800

    update

--- tr.py
+++ tr.py
@@ -7,36 +7,40 @@ data = []
 
 with open('list.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
+    data.sort(key=lambda x : int(x['id']), reverse=True)
 
 with open('list.json', 'w', encoding='utf-8') as f:
-    for item in data:
-        with open(f"{item['id']}/metrics.json", 'r', encoding='utf-8') as f2:
-            metrics = json.load(f2)
+    json.dump(data, f, indent=2, ensure_ascii=False)
 
-            item['annual_return'] = metrics['strategy']['annual_return_ratio']
-            item['sharpe_ratio'] = metrics['strategy']['sharpe_ratio']
+# with open('list.json', 'w', encoding='utf-8') as f:
+#     for item in data:
+#         with open(f"{item['id']}/metrics.json", 'r', encoding='utf-8') as f2:
+#             metrics = json.load(f2)
 
-        with open(f"{item['id']}/statistic.json", 'r', encoding='utf-8') as f3:
-            statistic = json.load(f3)
+#             item['annual_return'] = metrics['strategy']['annual_return_ratio']
+#             item['sharpe_ratio'] = metrics['strategy']['sharpe_ratio']
 
-            item['max_retracement'] = statistic['dollar_retracement']
+#         with open(f"{item['id']}/statistic.json", 'r', encoding='utf-8') as f3:
+#             statistic = json.load(f3)
 
-        with open(f"{item['id']}/price.json", 'r', encoding='utf-8') as f4:
-            prices = json.load(f4)
+#             item['max_retracement'] = statistic['dollar_retracement']
 
-            start_ts = prices[0][0] / 1000
-            end_ts = prices[-1][0] / 1000
+#         with open(f"{item['id']}/price.json", 'r', encoding='utf-8') as f4:
+#             prices = json.load(f4)
 
-            item['start_point'] = datetime.utcfromtimestamp(start_ts).strftime('%Y-%m-%d')
-            item['end_point'] = datetime.utcfromtimestamp(end_ts).strftime('%Y-%m-%d')
+#             start_ts = prices[0][0] / 1000
+#             end_ts = prices[-1][0] / 1000
 
-        dt = item['date']
-        current_id = item['id']
-        target_id = f"{''.join(dt.split('-'))}001"
+#             item['start_point'] = datetime.utcfromtimestamp(start_ts).strftime('%Y-%m-%d')
+#             item['end_point'] = datetime.utcfromtimestamp(end_ts).strftime('%Y-%m-%d')
 
-        os.rename(current_id, target_id)
-        item['id'] = target_id
+#         dt = item['date']
+#         current_id = item['id']
+#         target_id = f"{''.join(dt.split('-'))}001"
 
-    with open(f'list.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+#         os.rename(current_id, target_id)
+#         item['id'] = target_id
+
+#     with open(f'list.json', 'w', encoding='utf-8') as f:
+#         json.dump(data, f, indent=2, ensure_ascii=False)
 

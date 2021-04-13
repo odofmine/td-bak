d669cc1cad02a4a692645650517633f80c048b66
anonymouse <anonymouse@x.com>
2021-04-13 15:07:28 +0800

    update

--- tr.py
+++ tr.py
@@ -7,17 +7,11 @@ with open('list.json', 'r', encoding='utf-8') as f:
 
 with open('list.json', 'w', encoding='utf-8') as f:
     for item in data:
-        item['strategy'] = {
-            'name': 't4',
-            'version': '1.0'
-        }
-        item['source'] = 'bitmex-bitstamp'
-        item['start_point'] = '2014-01-01'
-        item['end_point'] = '2021-01-01'
-        item['frequency'] = '1d'
-        item['year_return'] = '23%'
-        item['max_retracement'] = '23%'
-        item['sharpe_ratio'] = '1.2'
+        with open(f"{item['id']}/metrics.json", 'r', encoding='utf-8') as f2:
+            metrics = json.load(f2)
+
+            item['annual_return'] = metrics['strategy']['annual_return_ratio']
+            item['sharpe_ratio'] = metrics['strategy']['sharpe_ratio']
 
     with open(f'list.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)

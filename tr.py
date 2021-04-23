c982d4c01d5b297171dce7d9963f31d4dd7ed462
anonymouse <anonymouse@x.com>
2021-04-23 15:13:56 +0800

    update

--- tr.py
+++ tr.py
@@ -21,16 +21,17 @@ base = {
 
 data = []
 
-for d in dirs:
-    ts = d[2:-1]
-    dt = datetime.fromtimestamp(int(ts))
+dirs = sorted(list(map(lambda x : int(x[2:-1]), dirs)))
+
+for ts in dirs:
+    dt = datetime.fromtimestamp(ts)
 
     item = {}
 
-    item['id'] = ts
+    item['id'] = str(ts)
     item['date'] = str(dt)
 
-    with open(f"{d}price.json", 'r', encoding='utf-8') as f4:
+    with open(f"{ts}/price.json", 'r', encoding='utf-8') as f4:
         prices = json.load(f4)
 
         start_ts = prices[0][0] / 1000
@@ -40,13 +41,13 @@ for d in dirs:
         item['end_point'] = datetime.utcfromtimestamp(end_ts).strftime('%Y-%m-%d')
 
 
-    with open(f"{d}metrics.json", 'r', encoding='utf-8') as f2:
+    with open(f"{ts}/metrics.json", 'r', encoding='utf-8') as f2:
         metrics = json.load(f2)
 
         item['annual_return'] = metrics['strategy']['annual_return_ratio']
         item['sharpe_ratio'] = metrics['strategy']['sharpe_ratio']
 
-    with open(f"{d}statistic.json", 'r', encoding='utf-8') as f3:
+    with open(f"{ts}/statistic.json", 'r', encoding='utf-8') as f3:
         statistic = json.load(f3)
 
         item['max_retracement'] = statistic['dollar_retracement']
@@ -56,4 +57,3 @@ for d in dirs:
 
 with open(f'list.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=2, ensure_ascii=False)
-

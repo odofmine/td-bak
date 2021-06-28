28f1a9ae4f0cab16ca7351fba12da1edccf8a07f
anonymouse <anonymouse@x.com>
2021-06-28 15:34:22 +0800

    update

--- scripts/tr-bt.py
+++ scripts/tr-bt.py
@@ -1,17 +1,12 @@
 import json
 import glob
 
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 
 dirs = glob.glob('./bt/*/')
 
 base = {
-    "strategy": {
-      "name": "trend-m-1",
-      "version": "1.0"
-    },
     "type": "bt",
-    "source": "bitmex-bitstamp",
     "frequency": "1d",
     "currency": "btc",
     "product": "perp",
@@ -34,8 +29,8 @@ for ts in dirs:
 
     base_dir = f'bt/{ts}'
 
-    with open(f"{base_dir}/price.json", 'r', encoding='utf-8') as f4:
-        prices = json.load(f4)
+    with open(f"{base_dir}/price.json", 'r', encoding='utf-8') as f1:
+        prices = json.load(f1)
 
         start_ts = prices[0][0]
 
@@ -46,8 +41,8 @@ for ts in dirs:
                 json.dump(data, f, indent=2, ensure_ascii=False)
 
 
-    with open(f"{base_dir}/price.json", 'r', encoding='utf-8') as f4:
-        prices = json.load(f4)
+    with open(f"{base_dir}/price.json", 'r', encoding='utf-8') as f2:
+        prices = json.load(f2)
 
         start_ts = prices[0][0]
         end_ts = prices[-1][0]
@@ -56,16 +51,36 @@ for ts in dirs:
         item['end_point'] = datetime.utcfromtimestamp(end_ts).strftime('%Y-%m-%d')
 
 
-    with open(f"{base_dir}/metrics.json", 'r', encoding='utf-8') as f2:
-        metrics = json.load(f2)
+    with open(f"{base_dir}/metrics.json", 'r', encoding='utf-8') as f3:
+        metrics = json.load(f3)
 
         item['annual_return'] = metrics['strategy']['annual_return_ratio']
         item['sharpe_ratio'] = metrics['strategy']['sharpe_ratio']
 
-    with open(f"{base_dir}/statistic.json", 'r', encoding='utf-8') as f3:
-        statistic = json.load(f3)
+
+    with open(f"{base_dir}/statistic.json", 'r', encoding='utf-8') as f4:
+        statistic = json.load(f4)
 
         item['max_retracement'] = statistic['dollar_retracement']
+
+    with open(f"{base_dir}/params.json", 'r', encoding='utf-8') as f5:
+        params = json.load(f5)
+
+        if 'pair' in params:
+            item['source'] = params['pair']
+        else:
+            item['source'] = 'btcusd'
+
+        if 'pstr' in params:
+            item['strategy'] = {
+                'name': params['pstr'],
+                'version': '1.0'
+            }
+        else:
+            item['strategy'] = {
+                'name': 'trend-m-1',
+                'version': '1.0'
+            }
 
     item = {**item, **base}
     data.append(item)

4d03be4e5b22066a35bcf65de7383f095b5e91c9
anonymouse <anonymouse@x.com>
2021-06-29 16:23:30 +0800

    update

---
+++ clean.py
@@ -0,0 +1,29 @@
+import json
+import glob
+import shutil
+
+from datetime import datetime, timedelta
+
+dirs = glob.glob('./fof/*/')
+
+dirs = [x[2:-1] for x in dirs]
+dirs = sorted(list(map(lambda x : int(x.split('/')[1]), dirs)), reverse=True)
+
+for ts in dirs:
+    dt = datetime.utcfromtimestamp(ts)
+    dt += timedelta(hours=8)
+
+    item = {}
+
+    id = str(ts)
+    date = str(dt)
+
+    base_dir = f'fof/{ts}'
+
+    with open(f"{base_dir}/metrics.json", 'r', encoding='utf-8') as f:
+        metrics = json.load(f)['fof']
+
+        sharpe_ratio = metrics['sharpe_ratio']
+        if sharpe_ratio < 2:
+          print(sharpe_ratio)
+          shutil.rmtree(f'xx/td/fof/{id}')

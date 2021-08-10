d24c07544002208f94418bd2b6477da5792ad23e
anonymouse <anonymouse@x.com>
2021-08-10 22:48:39 +0800

    update

--- clean.py
+++ clean.py
@@ -4,25 +4,20 @@ import shutil
 
 from datetime import datetime, timedelta
 
-dirs = glob.glob('./bt/*/')
+dirs = glob.glob('./fof/*/')
 
 dirs = [x[2:-1] for x in dirs]
 dirs = sorted(list(map(lambda x : int(x.split('/')[1]), dirs)), reverse=True)
 
+bts = 1628240933
+
 for ts in dirs:
     dt = datetime.utcfromtimestamp(ts)
-    dt += timedelta(hours=8)
 
-    item = {}
+    if ts < bts:
+        continue
 
-    id = str(ts)
-    date = str(dt)
+    base_dir = f'xxxx/td/fof/{ts}'
 
-    base_dir = f'xxx/bt/{ts}'
-
-    with open(f"{base_dir}/params.json", 'r', encoding='utf-8') as f:
-        metrics = json.load(f)
-
-        if not 'pair' in metrics or not 'pstr' in metrics:
-          print(dt)
-          shutil.rmtree(base_dir)
+    print(f'remove {base_dir}({dt})')
+    shutil.rmtree(base_dir)

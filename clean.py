22ae35e7b16f659fc9a51318018c89f3f4243764
anonymouse <anonymouse@x.com>
2021-12-08 20:14:03 +0800

    update

--- clean.py
+++ clean.py
@@ -9,13 +9,13 @@ dirs = glob.glob('./fof/*/')
 dirs = [x[2:-1] for x in dirs]
 dirs = sorted(list(map(lambda x : int(x.split('/')[1]), dirs)), reverse=True)
 
-start = 1626945267
+start = 1634625570
 end = 1626950546
 
 for ts in dirs:
     dt = datetime.utcfromtimestamp(ts)
 
-    if ts > end or ts < start:
+    if ts >= start:
         continue
 
     base_dir = f'/Users/lizhe/Workspace/td/fof/{ts}'

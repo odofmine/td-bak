994d408b5a0782633a4bf3dece267b90a96f8c7e
anonymouse <anonymouse@x.com>
2021-12-09 20:35:05 +0800

    update

--- clean.py
+++ clean.py
@@ -18,10 +18,14 @@ for ts in dirs:
     base_dir = f'/Users/lizhe/Workspace/td/fof/{ts}'
 
     with open(f'{base_dir}/remark.md', 'w') as f:
-        f.write('# 备注\n')
-        f.write('## 备注\n')
-        f.write('### 备注\n')
-        f.write('#### 备注\n')
+        f.write('#### 思路\n')
+        f.write('回测目的，要验证的东西\n')
+        f.write('#### 参数\n')
+        f.write('都有哪些可设置的参数以及固定的参数\n')
+        f.write('#### 描述\n')
+        f.write('描述结果\n')
+        f.write('#### 总结\n')
+        f.write('对于目的的总结，是否符合预期，或者是否有新的发现\n')
 
     # print(f'remove {base_dir}({dt})')
     # shutil.rmtree(base_dir)
